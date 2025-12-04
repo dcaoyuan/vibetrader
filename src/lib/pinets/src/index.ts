d906641151282dfa1e405edac5f5e8b7795f2694
Caoyuan Deng <dcaoyuan@gmail.com>
2025-12-04 14:57:45 -0800

    Removed pinets directory, will try to import this module.

--- src/lib/pinets/src/index.ts
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0-only
-// Copyright (C) 2025 Ala-eddine KADDOURI
-
-import PineTS from './PineTS.class';
-import { Context } from './Context.class';
-import { Provider } from './marketData/Provider.class';
-
-export { PineTS, Context, Provider };
--- src/lib/pinets/src/marketData/Provider.class.ts
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0-only
-
-import { BinanceProvider } from './Binance/BinanceProvider.class';
-
-export const Provider = {
-    Binance: new BinanceProvider(),
-    //TODO : add other providers (polygon, etc.)
-};
