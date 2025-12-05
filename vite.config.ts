4eb4413c15515c1ba02b0fbd3166c39617bb2016
Caoyuan Deng <dcaoyuan@gmail.com>
2025-12-04 22:42:57 -0800

    tab size = 4

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/vibetrader/',
-  build: {
-    sourcemap: true,
-  }
+    plugins: [react()],
+    base: '/vibetrader/',
+    build: {
+        sourcemap: true,
+    }
 })
